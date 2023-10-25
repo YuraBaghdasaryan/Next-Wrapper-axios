3cda8e0797105d72d88e0434e2e2c3880b633d67
YuraBaghdasaryan <yurabaghdasaryan889@gmail.com>
2023-10-25 18:07:53 +0400

    app

---
+++ app/store.ts
@@ -0,0 +1,29 @@
+import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import axios from 'axios';
+import exp from 'constants';
+import { createWrapper } from 'next-redux-wrapper';
+import productSlice from '@/features/product/productSlice';
+
+
+export const store =()=> configureStore({
+  reducer: {prod:productSlice
+    
+  },
+});
+
+export type AppStore=ReturnType<typeof store>
+export type AppDispatch = AppStore['dispatch'];
+export type RootState = ReturnType<AppStore['getState']>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+ReturnType, 
+RootState,
+unknown,
+Action<string>
+>;
+export const myAxios=axios.create({
+  baseURL:'https://fakestoreapi.com/'
+})
+
+
+export const wrapper=createWrapper<AppStore>(store)
+
